db70de78b0533ea46c2e464112e2c457c418c67f
Elvis Wanjiru <elvis.gatukui@student.moringaschool.com>
2022-08-23 23:17:25 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(callback){
+     callback("Spy");
+}
+receivesAFunction()  
+
+function returnsANamedFunction(){
+    return function namedFn (){
+           console.log("a named function")
+    }
+}
+function returnsAnAnonymousFunction(){
+    return () => console.log('anonymous function');
+}
